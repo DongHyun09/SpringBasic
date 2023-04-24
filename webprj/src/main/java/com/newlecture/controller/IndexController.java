28f2a90b5d9773df6455e0287ff2ee9d73b28cfa
SAMSUNG <SAMSUNG@222.109.20.115>
2023-04-24 23:19:00 +0900

    6강~17강(IndexController ~ Tiles ViewResolver)

---
+++ webprj/src/main/java/com/newlecture/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.newlecture.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+public class IndexController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		System.out.println("test");
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("data", "Hello Spring MVC");
+//		mav.setViewName("WEB-INF/view/index.jsp");
+
+		return mav;
+	}
+}
