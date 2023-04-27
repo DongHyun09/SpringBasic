927bc53afef820f0781e9291b4dddaaee3458881
SAMSUNG <SAMSUNG@125.129.0.5>
2023-04-28 00:29:40 +0900

    스프링 ~ 32강까지 복습 및 실습

--- webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
+++ webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -1,34 +1,33 @@
-package com.newlecture.web.controller.notice;
-
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
-
-import com.newlecture.web.entity.Notice;
-import com.newlecture.web.service.NoticeService;
-
-public class ListController implements Controller{
-	
-	
-	@Autowired
-	private NoticeService noticeService;
-	
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		
-		ModelAndView mav = new ModelAndView("notice.list");
-		List<Notice> list = noticeService.getList(1, "TITLE", "");
-		mav.addObject("list", list);
-		
-		return mav;
-	}
-
-	
-	 
-}
+/*
+ * package com.newlecture.web.controller.notice;
+ * 
+ * import java.util.List;
+ * 
+ * import javax.servlet.http.HttpServletRequest; import
+ * javax.servlet.http.HttpServletResponse;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.web.servlet.ModelAndView; import
+ * org.springframework.web.servlet.mvc.Controller;
+ * 
+ * import com.newlecture.web.entity.Notice; import
+ * com.newlecture.web.service.NoticeService;
+ * 
+ * public class ListController implements Controller{
+ * 
+ * 
+ * @Autowired private NoticeService noticeService;
+ * 
+ * @Override public ModelAndView handleRequest(HttpServletRequest request,
+ * HttpServletResponse response) throws Exception {
+ * 
+ * 
+ * ModelAndView mav = new ModelAndView("notice.list"); List<Notice> list =
+ * noticeService.getList(1, "TITLE", ""); mav.addObject("list", list);
+ * 
+ * return mav; }
+ * 
+ * 
+ * 
+ * }
+ */
