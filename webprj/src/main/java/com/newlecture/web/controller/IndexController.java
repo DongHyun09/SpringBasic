564998ca54b901cdd48cc16416682d36eed9153a
SAMSUNG <SAMSUNG@222.109.20.115>
2023-04-27 23:56:46 +0900

    26일 이론 -> 실습.

--- webprj/src/main/java/com/newlecture/web/controller/IndexController.java
+++ webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -1,19 +1,13 @@
 package com.newlecture.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+@Controller
+public class IndexController {
 
-public class IndexController implements Controller{
-
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		ModelAndView mav = new ModelAndView("root.index");	
-		return mav;
-	}
-	
-
+      @RequestMapping("/index")
+      public void aaaa() {
+              System.out.println("asdfg");
+}
 }
