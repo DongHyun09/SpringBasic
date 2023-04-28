9ac67a1afc3d3ee170b57b73d2ef339e950794f5
SAMSUNG <SAMSUNG@125.129.0.5>
2023-04-29 00:35:25 +0900

    NewLecture 32~47(완강)

--- webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -1,13 +1,12 @@
 package com.newlecture.web.controller.customer;
 
 import java.sql.SQLException;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
 
 @Controller
@@ -18,13 +17,16 @@ public class NoticeController{
 	private NoticeService noticeService;
 
     @RequestMapping("list") 
-    public String list() throws ClassNotFoundException, SQLException{
-     
-    List<Notice> list = noticeService.getList(1, "TITLE", "");                  
+    public String list(@RequestParam(name="p", defaultValue = "1") Integer page) throws ClassNotFoundException, SQLException{
+    	//String p = request.getParameter("p");
+    	System.out.println("page:"+page);
+    	
+		/* List<Notice> list = noticeService.getList(1, "TITLE", ""); */                  
             return "notice.list";
 }
     @RequestMapping("detail") 
     public String detail(){
+    	
      return "notice.detail";
 }
 }
